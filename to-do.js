cfd2fa72696deef18d0e4ece4602dec952e0b6f9
hiba_khadir <khadirhiba0745@gmail.com>
2025-04-14 23:38:14 +0100

    added save task and text wrap

--- to-do.js
+++ to-do.js
@@ -1,6 +1,3 @@
-let tasksArray = [];
-let num;
-
 
 //add a task 
 function addTask(){
@@ -10,7 +7,7 @@ function addTask(){
                                 <button     class="check-button"    onclick="checkTask(this)">
                                     <img  class= "check-box" src="icons/check-box-off.svg" >
                                 </button>
-                                <input placeholder="Enter The New Task" class="input-task"  type="text"> 
+                                <input placeholder="Enter The New Task" class="input-task"  type="text" onclick="saveTask(this)"> 
                               
                                 <button class="delete-task" onclick="deleteTask(this)">
                                     <img class = "delete-task-img" src="icons/delete.svg">
@@ -21,6 +18,27 @@ function addTask(){
     let taskList = document.getElementById('js-tasks');    
     taskList.innerHTML += taskHolderHTML ;
 }
+
+//save the task entered
+function saveTask(inputElement){
+    const newTask = inputElement.closest(".task"); //get the task added 
+
+    inputElement.addEventListener('keydown',
+        (event)=>{//change the input element to a text 
+            if (event.key === "Enter" && inputElement.value !== '') {
+                newTask.innerHTML = `<button     class="check-button"    onclick="checkTask(this)">
+                                        <img  class= "check-box" src="icons/check-box-off.svg" >
+                                    </button> 
+                                    <p class ="task-txt">${inputElement.value}</p>
+                                    <button class="delete-task" onclick="deleteTask(this)">
+                                        <img class = "delete-task-img" src="icons/delete.svg">
+                                    </button>`;
+                }
+                newTask.style.display = "grid";
+                newTask.style.gridTemplateColumns = "1fr 8fr 1fr";
+                newTask.querySelector('.task-txt').style.display = "block";         
+            })
+        }     
 
 //check or uncheck a tasks  
 function checkTask(button){
